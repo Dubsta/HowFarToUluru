4484007ff61722b7878dab7068b7d9ae9b2f4bce
Michael Atkinson <mikeatk88@gmail.com>
2021-09-21 21:39:59 +1000

    Add feature to draw line
    + Draw a line from Uluru to the marker
    + Update data and line on drag

--- main.js
+++ main.js
@@ -51,126 +51,63 @@ const marker = new mapboxgl.Marker({
 .setLngLat(uluruLngLat)
 .addTo(map);
  
+// Data for to draw line
+const linegeojson = {
+      'type': 'Feature',
+      'properties': {},
+      'geometry': {
+          'type': 'LineString',
+          'coordinates': [
+              uluruGeojson.geometry.coordinates,
+              uluruGeojson.geometry.coordinates
+          ]
+      }
+};
 
 function updateMarker() {
-  // Get the new distance and update the results
+  // Redraw the line, get the new distance, and update the results
+  
+  //Redraw line
   const markerLngLat = marker.getLngLat();
+  linegeojson.geometry.coordinates.pop();
+  linegeojson.geometry.coordinates.push(Object.values(markerLngLat));
+  map.getSource('line').setData(linegeojson);
+
+  //update distance
   const distanceTo = markerLngLat.distanceTo(uluruLngLat);
   const results = document.getElementById('results');
   const distanceStr = `${Math.round(distanceTo/1000).toLocaleString("en-US")}km`
   document.getElementById('distance').innerHTML = distanceStr;
-  //const description = document.getElementById('description');
   if (results.style.display === "none") results.style.display = "block";
 }
 
 // Handlers
+
+map.on('load', () => {
+    map.addSource('line', {
+        'type': 'geojson',
+        'data': linegeojson
+    });
+    map.addLayer({
+        'id': 'line',
+        'type': 'line',
+        'source': 'line',
+        'layout': {
+            'line-join': 'round',
+            'line-cap': 'round'
+        },
+        'paint': {
+            'line-color': '#888',
+            'line-width': 8
+        }
+    });
+});
+
+marker.on('drag', updateMarker);
+
 marker.on('dragend', updateMarker);
 
 map.on('click', (e) => {
   marker.setLngLat(e.lngLat);
   updateMarker();
-});
-
-
-// Draw a line between Uluru and user marker
-
-// GeoJSON object to hold our measurement features
-
-// const geojson = {
-// 'type': 'FeatureCollection',
-// 'features': []
-};
- 
-// Used to draw a line between points
-const linestring = {
-  'type': 'Feature',
-  'geometry': {
-    'type': 'LineString',
-    'coordinates': []
-  }
-};
- 
-// map.on('load', () => {
-//   map.addSource('geojson', {
-//     'type': 'geojson',
-//     'data': geojson
-//});
- 
-// Add styles to the map
-map.addLayer({
-  id: 'measure-line',
-  type: 'line',
-  source: 'geojson',
-  layout: {
-    'line-cap': 'round',
-    'line-join': 'round'
-  },
-  paint: {
-    'line-color': '#000',
-    'line-width': 2.5
-    //'line-dasharray': 1;
-  },
-  filter: ['in', '$type', 'LineString']
-});
- 
-map.on('click', (e) => {
-  const features = map.queryRenderedFeatures(e.point, {
-    layers: ['measure-points']
-});
- 
-// Remove the linestring from the group
-// so we can redraw it based on the points collection.
-if (geojson.features.length > 1) geojson.features.pop();
- 
-// Clear the distance container to populate it with a new value.
-distanceContainer.innerHTML = '';
- 
-// If a feature was clicked, remove it from the map.
-if (features.length) {
-const id = features[0].properties.id;
-geojson.features = geojson.features.filter(
-(point) => point.properties.id !== id
-);
-} else {
-const point = {
-'type': 'Feature',
-'geometry': {
-'type': 'Point',
-'coordinates': [e.lngLat.lng, e.lngLat.lat]
-},
-'properties': {
-'id': String(new Date().getTime())
-}
-};
- 
-geojson.features.push(point);
-}
- 
-if (geojson.features.length > 1) {
-linestring.geometry.coordinates = geojson.features.map(
-(point) => point.geometry.coordinates
-);
- 
-geojson.features.push(linestring);
- 
-// Populate the distanceContainer with total distance
-const value = document.createElement('pre');
-const distance = turf.length(linestring);
-value.textContent = `Total distance: ${distance.toLocaleString()}km`;
-distanceContainer.appendChild(value);
-}
- 
-map.getSource('geojson').setData(geojson);
-});
-});
- 
-map.on('mousemove', (e) => {
-const features = map.queryRenderedFeatures(e.point, {
-layers: ['measure-points']
-});
-// Change the cursor to a pointer when hovering over a point on the map.
-// Otherwise cursor is a crosshair.
-map.getCanvas().style.cursor = features.length
-? 'pointer'
-: 'crosshair';
 });
